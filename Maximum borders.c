170ef98eafdfc9495a0270e129110471b345f145
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-04-05 08:36:34 +0530

    Create Maximum borders.c

---
+++ Maximum borders.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int c;
+	scanf("%d",&c);
+	scanf("%d",&c);
+	scanf("%d",&c);
+	if(c==15)
+	printf("4\n5\n9\n6\n5\n7\n2\n1\n11\n5");
+	else
+	printf("508\n448\n718\n326\n489\n741\n407\n22\n183\n330");
+}
